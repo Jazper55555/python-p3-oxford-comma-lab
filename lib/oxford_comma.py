fe83b18be6cb7b71a87a73fd8f313d63b66bfc82
Jazper <jazpersaldana@yahoo.com>
2024-05-25 16:17:05 -0500

    oxford comma lab complete

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,13 @@
+# oxford_comma(['Luca', 'Andrea', 'Stew'])
+
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return ''.join(items)
+    elif len(items) == 2:
+        return ' and '.join(items)
+    elif len(items) >= 3:
+        items.insert(-1, f'and {items[-1]}')
+        items.pop()
+        string = ', '.join(items)
+        return string
+    pass
